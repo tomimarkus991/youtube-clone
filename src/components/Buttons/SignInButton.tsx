da17cb0b459ce5dd20b1b76074d765c061167961
tomimarkus991 <tomimarkusalber@gmail.com>
2021-03-16 09:32:10 +0200

    restyle sign in button

--- src/components/Buttons/SignInButton.tsx
+++ src/components/Buttons/SignInButton.tsx
@@ -1,15 +1,20 @@
 import { Button } from "@chakra-ui/button";
+import { useColorModeValue } from "@chakra-ui/color-mode";
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 
 interface SignInButtonProps {}
 
 export const SignInButton: React.FC<SignInButtonProps> = ({}) => {
+  const scheme = useColorModeValue("#2B76DA", "#3EA6FF");
   return (
     <Button
       leftIcon={<FaUserCircle size={25} />}
+      // ringColor="telegram.900"
       colorScheme="blue"
       variant="outline"
+      textColor={scheme}
+      borderColor={scheme}
     >
       SIGN IN
     </Button>
