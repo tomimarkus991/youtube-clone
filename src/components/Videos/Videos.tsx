bbd443eeff1ee25160445b8c8f21b82185db3243
tomimarkus991 <tomimarkusalber@gmail.com>
2021-03-16 11:07:11 +0200

    add infinite scroll

--- src/components/Videos/Videos.tsx
+++ src/components/Videos/Videos.tsx
@@ -1,45 +1,67 @@
-import { Grid } from "@chakra-ui/layout";
+import { Flex, Grid } from "@chakra-ui/layout";
 import React, { useEffect, useState } from "react";
-import { getVideos } from "../../utils/getData";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { getNextVideos, getVideos } from "../../utils/getData";
 import { Video } from "./Video";
 
 interface VideosProps {}
 
 export const Videos: React.FC<VideosProps> = ({}) => {
-  // react scroll pagination
-  // what stuff bar
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<any>([]);
+  const [nextPageToken, setNextPageToken] = useState();
   const up = async () => {
-    let data = await getVideos();
-    setData(data.items);
+    let response = await getVideos();
+
+    setData(response.items);
+    setNextPageToken(response.nextPageToken);
+  };
+  const next = async () => {
+    if (data) {
+      let data = await getNextVideos(nextPageToken);
+
+      setData((oldState: any) => [...oldState, ...data.items]);
+      setNextPageToken(data.nextPageToken);
+    }
   };
   useEffect(() => {
     up();
   }, []);
   return (
-    <Grid
+    <Flex
       boxSizing="border-box"
       marginY="1em"
       marginX={{ base: "1em", xl: "1.5em", xsl: "1em", xxl: "4.5em" }}
-      templateRows={{
-        sm: "repeat(1, 1fr)",
-        md: "repeat(2, 1fr)",
-        lg: "repeat(3, 1fr)",
-        xl: "repeat(4, 1fr)",
-      }}
-      templateColumns={{
-        sm: "repeat(1, 1fr)",
-        md: "repeat(2, 1fr)",
-        lg: "repeat(3, 1fr)",
-        xl: "repeat(4, 1fr)",
-      }}
-      gap={4}
       w="100%"
-      justifyItems="center"
+      justifyContent="center"
     >
-      {data.map((video: any) => (
-        <Video video={video} id={video.id} />
-      ))}
-    </Grid>
+      <InfiniteScroll
+        dataLength={data.length}
+        next={next}
+        hasMore={true}
+        loader={<div></div>}
+        endMessage={<div></div>}
+        scrollThreshold={0.9}
+      >
+        <Grid
+          templateRows={{
+            sm: "repeat(1, 1fr)",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(3, 1fr)",
+            xl: "repeat(4, 1fr)",
+          }}
+          templateColumns={{
+            sm: "repeat(1, 1fr)",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(3, 1fr)",
+            xl: "repeat(4, 1fr)",
+          }}
+          gap={4}
+        >
+          {data.map((video: any) => (
+            <Video video={video} id={video.id} />
+          ))}
+        </Grid>
+      </InfiniteScroll>
+    </Flex>
   );
 };
--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -6,6 +6,16 @@ export const getVideos = async () => {
   return data;
 };
 
+export const getNextVideos = async (next: any) => {
+  console.log("token", next);
+
+  let response = await fetch(
+    `https://www.googleapis.com/youtube/v3/videos?key=${process.env.NEXT_PUBLIC_API_KEY}&chart=mostPopular&maxResults=20&part=snippet,status,contentDetails,statistics&pageToken=${next}`
+  );
+  let data = await response.json();
+  return data;
+};
+
 export const getUserProfile = async (id: string) => {
   let response = await fetch(
     `https://www.googleapis.com/youtube/v3/channels?key=${process.env.NEXT_PUBLIC_API_KEY}&part=snippet&id=${id}`
